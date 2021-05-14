d85e6cf7baef9f8b9c636c80c0aae7b3f1568667
Avani Patel <avani.shah1807@gmail.com>
2021-05-14 09:36:25 +0200

    Adds /watches endpoint to view the list of watches available for download

--- src/main/java/com/bcg/watch/api/service/WatchCatalogueService.java
+++ src/main/java/com/bcg/watch/api/service/WatchCatalogueService.java
@@ -1,11 +1,17 @@
 package com.bcg.watch.api.service;
 
+import com.bcg.watch.api.exception.WatchIdAlreadyExistsException;
+import com.bcg.watch.api.model.Watch;
+import com.bcg.watch.api.model.WatchCatalogue;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "watch")
@@ -17,6 +23,20 @@ public class WatchCatalogueService {
     @Produces("text/PLAIN")
     public String getHealth() {
         return "Status: OK";
+    }
+
+    @GET
+    @Path("/watches")
+    @Produces("application/json")
+    public Response getWatch() {
+        List<Watch> watches = null;
+        try {
+            watches = WatchCatalogue.getInstance().getAllWatches();
+        } catch (WatchIdAlreadyExistsException e) {
+            e.printStackTrace();
+            return Response.status(400).build();
+        }
+        return Response.status(200).entity(watches).build();
     }
 
 }
